50416aa572e136aed96071d4d30e003a0edb4468
Kiran Rana <kiranrana8973@gmail.com>
2023-10-09 09:32:22 +0545

    Added routes

---
+++ lib/app/routes/app_route.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_batch31b/view/arithmetic_view.dart';
+import 'package:flutter_batch31b/view/dashboard_view.dart';
+
+class AppRoute {
+  AppRoute._();
+
+  static const String dashboardRoute = '/';
+  static const String arithmeticRoute = '/arithmetic';
+  static const String outputRoute = '/output';
+
+  static getApplicationRoute() {
+    return {
+      dashboardRoute: (context) => const DashboardView(),
+      // outputRoute: (context) => const OutputView(),
+      arithmeticRoute: (context) => const AtithmeticView(),
+    };
+  }
+}
